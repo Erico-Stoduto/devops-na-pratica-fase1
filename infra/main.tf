65be007ac5bb38bd9781d81c5c41ba3bf1cf7c79
Erico Stoduto <104444903+Erico-Stoduto@users.noreply.github.com>
2025-10-18 16:44:03 -0300

    Add Terraform configuration for AWS S3 bucket

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+# main.tf - Infraestrutura como Código (IaC)
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Criação de um bucket S3 para hospedar o site
+resource "aws_s3_bucket" "devops_site" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  tags = {
+    Name        = "DevOpsNaPratica"
+    Environment = "Fase1"
+  }
+}
