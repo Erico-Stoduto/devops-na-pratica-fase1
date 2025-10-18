ae6e4ea866d9e91a8a3477d765b4f2facf05cf38
Erico Stoduto <104444903+Erico-Stoduto@users.noreply.github.com>
2025-10-18 16:45:00 -0300

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+# variables.tf - Declaração de variáveis
+
+variable "region" {
+  description = "Região da AWS onde os recursos serão criados"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Nome do bucket S3"
+  default     = "devops-na-pratica-erico-site"
+}
